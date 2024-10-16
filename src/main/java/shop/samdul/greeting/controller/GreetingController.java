7402c5f1e257d9800ffdbdf862b1e7096d6a2fc4
GITSangWoo <dogcenta@gmail.com>
2024-10-16 15:33:09 +0900

    mine

--- src/main/java/shop/samdul/greeting/controller/GreetingController.java
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -1,7 +1,5 @@
-package shop.samdul.greeting.controller; //경로에 있을떈 패키지를 써주게 되어있음 
-
+package shop.samdul.greeting.controller;
 import java.util.Stack;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,28 +7,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    private Stack<People> nameStack = new Stack<>();
     Stack<String> names = new Stack<>();
+    Stack<People> ps = new Stack<>();  
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",required = false, defaultValue = "HI")String name, Model model){
         names.push(name);
         model.addAttribute("names", names);
         return "greeting";
     }
-    
-    @GetMapping("/people")	
-    public String people(@RequestParam(name="name", required=false, defaultValue="HI") String name, Model model) {
-		People p = new People();
-		if (!nameStack.isEmpty()) {
-			People pp = nameStack.peek();
-			p.setNum(pp.getNum() + 1);
-		} else {
-				p.setNum(1);
-		}
-		p.setName(name.toLowerCase());
-		nameStack.push(p);
-		model.addAttribute("nameStack", nameStack);
-		
-		return "greeting2";
-  }
+    @GetMapping("/people")
+    public String people(@RequestParam(name="name",required = false, defaultValue = "HI")String name, Model model){
+        People p = new People(0, name);
+        p.num=ps.size()+1;
+        ps.add(p);
+        model.addAttribute("people", ps);
+        return "greeting3";
+    }
+
 }
--- src/main/java/shop/samdul/greeting/controller/People.java
+++ src/main/java/shop/samdul/greeting/controller/People.java
@@ -1,22 +1,12 @@
 package shop.samdul.greeting.controller;
 
 public class People {
-  private int num;
-  private String name;
+    public int num;
+    public String name;
 
-  public int getNum() {
-    return num;
-  }
-
-  public void setNum(int num) {
-    this.num = num;
-  }
-
-  public String getName() {
-    return name;
-  }
-  
-  public void setName(String name) {
-    this.name = name;
-  }
+    People(int num, String name){
+        this.num = num;
+        this.name = name;
+    }
+    
 }
