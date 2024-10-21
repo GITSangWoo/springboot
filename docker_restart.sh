66103795e6d82d3fdfb6e4e0cd53c9d70eae099a
GITSangWoo <dogcenta@gmail.com>
2024-10-21 15:28:28 +0900

    fix restart

--- docker_restart.sh
+++ docker_restart.sh
@@ -7,6 +7,6 @@ echo "DOCKER RM"
 docker rm app
 
 echo "DOCKER RUN"
-docker run --name app -p 8080:8080 -d datamario24/greeting:latest
+docker run --name app -p 8080:8080 -d dogcenta/greeting:latest
 
 echo "DONE"
