3270ecd4b140b47eb9cacdd891bee3dc64dd76f2
GITSangWoo <dogcenta@gmail.com>
2024-10-22 16:23:16 +0900

    add func

---
+++ src/main/java/shop/samdul/greeting/mapper/TodoMapper.java
@@ -0,0 +1,12 @@
+package shop.samdul.greeting.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import shop.samdul.greeting.entity.TodoEntity;
+
+@Mapper
+public interface TodoMapper {
+    List<TodoEntity> findAll();
+}
